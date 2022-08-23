4c99c07f5c256842d53106fab5f2b1f0ac8201c1
ngoctrung2000 <trungngoc359@gmail.com>
2022-08-23 17:22:41 +0700

    lk don

---
+++ ds-lien-ket-don/Header.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "iostream"
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node* next;
+};
+
+struct LinkedList
+{
+	Node* head;
+	Node* tail;
+};
+
+Node* CreateNode(int init_data);
+void CreateList(LinkedList& l);
+void AddHead(LinkedList& l, Node* node);
+void AddTail(LinkedList& l, Node* node);
+void InsertAfterQ(LinkedList& l, Node* p, Node* q);
+int RemoveHead(LinkedList& l, int& x);
+int RemoveTail(LinkedList& l, int& x);
+int RemoveAfterQ(LinkedList& l, Node* q, int& x);
+Node* GetNode(LinkedList l, int index);
+void PrintList(LinkedList l);
+Node* Search(LinkedList l, int x);
+int Length(LinkedList l);
+void DestroyList(LinkedList& l);
